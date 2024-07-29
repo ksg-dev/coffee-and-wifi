5bbe8ab3eb4af201b551bf79fc807283372074d7
ksg-dev <ksg.dev.data@gmail.com>
2024-07-29 13:43:18 -0500

    add cafe class and render col headers

---
+++ cafes.py
@@ -0,0 +1,11 @@
+# Responsible for modeling cafe data
+
+class Cafe:
+    def __init__(self, name, location_link, open, close, coffee, wifi, power):
+        self.name = name
+        self.location_link = location_link
+        self.open = open
+        self.close = close
+        self.coffee = coffee
+        self.wifi = wifi
+        self.power = power
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import csv
+from cafes import Cafe
+from pprint import pprint
 
 '''
 Red underlines? Install the required packages first: 
@@ -58,10 +60,16 @@ def cafes():
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
+
         for row in csv_data:
             list_of_rows.append(row)
-    print(list_of_rows)
-    return render_template('cafes.html', cafes=list_of_rows)
+    headings = list_of_rows[0]
+    cafe_objs = []
+    for cafe in list_of_rows[1:]:
+        cafe_obj = Cafe(name=cafe[0], location_link=cafe[1], open=cafe[2], close=cafe[3], coffee=cafe[4], wifi=cafe[5], power=cafe[6])
+        cafe_objs.append(cafe_obj)
+        pprint(vars(cafe_obj))
+    return render_template('cafes.html', cafes=cafe_objs, heads=headings)
 
 
 if __name__ == '__main__':
